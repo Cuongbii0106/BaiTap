326e013cfdc9349baca7ddb7b67a28323df3c0f8
cuongnqhe173086 <cuongnqhe173086@fpt.edu.vn>
2025-09-23 11:22:30 +0700

    cau7

---
+++ Bai7/bai7.js
@@ -0,0 +1,120 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtuonElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+function startGame() {
+    console.log('Strarted')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtuonElement.appendChild(button)
+    })
+}
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtuonElement.firstChild) {
+        answerButtuonElement.removeChild(answerButtuonElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtuonElement.children).forEach(
+        button => {
+            setStatusClass(button, button.dataset.correct)
+        }
+    )
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        nextButton.classList.remove('hide')
+    }
+
+}
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+let score = 0;
+
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    if (correct) {
+        score++   
+    }
+    setStatusClass(document.body, correct)
+    Array.from(answerButtuonElement.children).forEach(
+        button => {
+            setStatusClass(button, button.dataset.correct)
+        }
+    )
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        nextButton.classList.remove('hide')
+
+        
+        questionElement.innerText = `Bạn được ${score} / ${shuffledQuestions.length} điểm`
+    }
+}
+
+let questions = [];
+
+fetch('bai7.json')
+  .then(res => {
+    if (!res.ok) throw new Error('HTTP error ' + res.status);
+    return res.json();
+  })
+  .then(data => {
+    questions = data;
+    console.log('Loaded questions:', questions.length);
+    // (tuỳ chọn) enable nút Start nếu bạn disable lúc đầu:
+    // startButton.disabled = false;
+  })
+  .catch(err => {
+    console.error('Failed to load questions.json', err);
+  });
