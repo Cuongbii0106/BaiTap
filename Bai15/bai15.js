d2c6347a0ea5efc797bba465257acf2a89c4ee8b
cuongnqhe173086 <cuongnqhe173086@fpt.edu.vn>
2025-10-02 12:44:01 +0700

    bai15

---
+++ Bai15/bai15.js
@@ -0,0 +1,35 @@
+const fileInput = document.getElementById("file-input");
+const container = document.getElementById("container");
+
+function previewFiles() {
+  container.innerHTML = ""; // clear cũ
+
+  const files = fileInput.files;
+
+  for (const file of files) {
+    //  validate loại file
+    if (!["image/jpeg", "image/png"].includes(file.type)) {
+      alert(`${file.name} không hợp lệ! Chỉ chọn JPG hoặc PNG.`);
+      continue;
+    }
+
+    //  validate
+    if (file.size > 2 * 1024 * 1024) {
+      alert(`${file.name} quá lớn! (chỉ < 2MB).`);
+      continue;
+    }
+
+    //  tạo preview
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const img = document.createElement("img");
+      img.src = e.target.result;
+      container.appendChild(img);
+    };
+    reader.readAsDataURL(file);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fileInput.addEventListener("change", previewFiles);
+});
